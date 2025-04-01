c95b4490524577bd39303d6e0e4447b6385048ec
Ivan Bensa <ivanb.webdev@proton.me>
2025-04-01 21:21:30 +0200

    Create script.js

---
+++ 06_htnl_js/script.js
@@ -0,0 +1,9 @@
+let name = prompt("Upisite vase ime").toLowerCase();
+
+let names = ["admin", "administrator", "toma"];
+
+    if (names.includes(name)) {
+      alert("Dobrodosao administratore");
+         } else {
+      alert("Nemate pristup sajtu");
+         }
